186f40f4c19fa72813d568d032f876cfa7b13e34
Yuvraj Singh <uvi58493@gmail.com>
2026-04-02 21:22:02 +0530

    Add 496_next_greater_element_i.java - LeetCode Problem 496

---
+++ 496_next_greater_element_i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[] result = new int[n];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = n - 1; i >= 0; i--) {
+            while(!stack.isEmpty() && nums2[stack.peek()] <= nums2[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? -1 : nums2[stack.peek()];
+            stack.push(i);
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            map.put(nums2[i], i);
+        }
+
+        int[] output = new int[m];
+        for(int i = 0 ; i < m; i++) {
+            int idx = map.get(nums1[i]);
+            output[i] = result[idx];
+        }
+        return output;
+    }
+}
